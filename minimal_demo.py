abfbc36e5d10af3148a688bcc4caa9f6c64de9fb
j-lago <jackson.lago@ifsc.edu.br>
2025-04-11 00:51:46 -0300

    add MinimalDemo

--- minimal_demo.py
+++ minimal_demo.py
@@ -1,7 +1,8 @@
-import pygame.font
+import lerp
 from basescreen import BaseScreen
 from canvas import Canvas
-
+from math import sin, cos, pi, fmod
+from lerp import lerp_vec3
 
 class MinimalDemo(BaseScreen):
     def __init__(self, *args, **kwargs):
@@ -9,7 +10,17 @@ class MinimalDemo(BaseScreen):
         self.canvases['main'] = Canvas(self.canvas_size, fonts=self.fonts, draw_fun=self.draw_main)
 
     def draw_main(self, canvas: Canvas):
-        canvas.draw_text((230, 180, 30), self.fonts['huge'], f'{self.t:.1f}s', (0, 0))
+        blue = (30, 60, 255)
+        red = (255, 30, 60)
+        yellow = (255, 200, 30)
+        for n in range(N := 400):
+            color = lerp_vec3(blue, red, n/N)
+            th = fmod(self.t*2, 2*pi)+3*pi*n/N
+            h = cos(self.t/2) + 2
+            r = cos(h*th) * 0.5
+            canvas.draw_circle(color, (r * cos(th), r * sin(th)), .08)
+        canvas.draw_text(yellow, self.fonts['huge'], f'{self.t:.1f}s', (0, 0))
+
 
 
 if __name__ == '__main__':
