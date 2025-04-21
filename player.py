cfa7e0e224af9f41a25f379c3a629cc26fbc1a60
j-lago <jackson.lago@ifsc.edu.br>
2025-04-21 09:02:31 -0300

    fix fps control

--- player.py
+++ player.py
@@ -103,7 +103,7 @@ class Cart:
             self.base_color, self.bg_color,
             border_width=2,
             border_radius=0,
-            show_particles=True,
+            show_particles=False,
         )
 
         self.point_particles = None
